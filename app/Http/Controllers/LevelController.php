a04a042961926cc87184a464a8e114d336d0a838
FelixYuda <yuda8675@gmail.com>
2024-08-12 22:23:30 +0700

    delete data m_level

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -8,17 +8,18 @@ use Illuminate\Support\Facades\DB;
 class LevelController extends Controller
 {
     public function index(){
-        DB::insert('insert into m_level(level_kode, level_nama, created_at) value(?, ?, ?)', 
-        ['CUS', 'Pelanggan', now()]);
+        // DB::insert('insert into m_level(level_kode, level_nama, created_at) value(?, ?, ?)', 
+        // ['CUS', 'Pelanggan', now()]);
 
-        return 'insert data baru berhasil';
+        // return 'insert data baru berhasil';
     
-        $row = DB::update('update m_level set level_nama = ? where level_kode = ?', 
-        ['Customer', 'CUS']);
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', 
+        // ['Customer', 'CUS']);
 
-        return 'update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
-    
-        
+        // return 'update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+
+        $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        return 'Delete data berhasil, Jumlah data yang dihapus: ' . $row . ' baris';
     
     }
 }
