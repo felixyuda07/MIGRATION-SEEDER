7138f41120784ae063043a1237325870acf2d0c1
FelixYuda <yuda8675@gmail.com>
2024-08-12 22:19:23 +0700

    update m_lavel

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -13,7 +13,10 @@ class LevelController extends Controller
 
         return 'insert data baru berhasil';
     
+        $row = DB::update('update m_level set level_nama = ? where level_kode = ?', 
+        ['Customer', 'CUS']);
 
+        return 'update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
     
     }
 }
