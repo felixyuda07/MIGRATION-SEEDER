a1ae885419667bc36832ef6b951673a892b95edb
FelixYuda <yuda8675@gmail.com>
2024-08-12 22:46:53 +0700

    update kategori

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -9,15 +9,17 @@ use Illuminate\Support\Facades\DB;
 class KategoriController extends Controller
 {
     public function index(){
-        $data = [
-            'kategori_kode'=>'SNK',
-            'level_nama'=>'Snack/Makanan RIngan',
-            'created_at'=>now()
-        ];
-        DB::table('m_kategori')->insert($data);
+        // $data = [
+        //     'kategori_kode'=>'SNK',
+        //     'level_nama'=>'Snack/Makanan RIngan',
+        //     'created_at'=>now()
+        // ];
+        // DB::table('m_kategori')->insert($data);
 
-        return 'data behasil ditambah';
+        // return 'data behasil ditambah';
 
+        $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['level_nama' => 'Camilan']);
+        return 'update data berhasil, jumlah data yang terupdate'  . $row . ' baris';
         
     }
 }
