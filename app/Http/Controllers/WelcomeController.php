2d27e03de792b1d1d65120ba0d821c315db41dbb
FelixYuda <yuda8675@gmail.com>
2024-08-15 11:24:03 +0700

    penerapan layouting

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Selamat Datang',
+            'list'  => ['Home', 'Welcome']
+        ];
+
+        $activeMenu = 'dashboard';
+
+        return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+    }
+
+}
