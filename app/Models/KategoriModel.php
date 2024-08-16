0f72a182417f138b16eb1255fdb7914023a74434
FelixYuda <yuda8675@gmail.com>
2024-08-16 22:54:57 +0700

    Kategori DataTable

---
+++ app/Models/KategoriModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriModel extends Model
+{
+    // Nama tabel yang sesuai dengan nama tabel di database
+    protected $table = 'm_kategori';
+    
+    // Kolom yang bisa diisi massal
+    protected $fillable = ['kategori_kode', 'kategori_nama']; // Ganti dengan kolom yang sesuai
+
+    // Jika tabel tidak memiliki kolom timestamp
+    public $timestamps = false;
+
+     // Menyebutkan kolom primary key
+     protected $primaryKey = 'kategori_id'; // Ganti dengan nama kolom primary key yang sesuai
+
+
+    // Tipe primary key (misalnya, string jika menggunakan UUID)
+    protected $keyType = 'string'; 
+
+    // Relasi dengan UserModel
+    public function users()
+    {
+        return $this->hasMany(UserModel::class, 'kategori_id');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\WelcomeController;
 use Illuminate\Routing\RouteGroup;
 
 
-Route::get('/kategori', [KategoriController::class, 'index']);
+
 
 Route::get('/', [WelcomeController::class, 'index']);
 Route::get('/barang', [BarangController::class, 'index']);
@@ -39,4 +39,15 @@ Route::Group(['prefix' => 'level'], function(){
     Route::get('/{id}/edit', [LevelController::class, 'edit']); //menampilkan halaman form edit
     Route::put('/{id}', [LevelController::class, 'update']); //menyimpan perubahan data user
     Route::delete('/{id}', [LevelController::class, 'destroy']); //menghapus data user
+});
+
+Route::Group(['prefix' => 'kategori'], function(){
+    Route::get('/', [KategoriController::class, 'index']); //menampilkan halaman awal user
+    Route::post('/list', [KategoriController::class, 'list']);  //menampilkan data user dalam bentuk json untuk datables
+    Route::get('/create', [KategoriController::class, 'create']); //menampilkan hallaman form tambah user
+    Route::post('/', [KategoriController::class, 'store']); //menyimpan data user baru
+    Route::get('/{id}', [KategoriController::class, 'show']); //menampilkan detail user
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']); //menampilkan halaman form edit
+    Route::put('/{id}', [KategoriController::class, 'update']); //menyimpan perubahan data user
+    Route::delete('/{id}', [KategoriController::class, 'destroy']); //menghapus data user
 });
